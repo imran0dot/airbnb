be30eded8f8086820af5e98a10ab211ff0d88ec2
imran0dot <imran0dot@gmail.com>
2024-01-04 23:22:21 +0600

    user next auth setup

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -6,13 +6,14 @@ import image from '@/public/images/airbnb.png'
 
 const Logo = () => {
     return (
-        <Image
-            alt='logo'
-            className='hidden md:block cursor-pointer'
-            height="100"
-            width="100"
-            src={image}
-        />
+        <Link href="/">
+            <Image
+                alt='logo'
+                className='hidden md:block cursor-pointer'
+                height="100"
+                width="100"
+                src={image}
+            /></Link>
     );
 };
 
